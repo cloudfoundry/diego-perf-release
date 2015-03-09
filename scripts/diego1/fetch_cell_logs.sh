7203bfd1474f4bce460683a41d90104ba713488d
Amit Gupta <agupta@pivotal.io>
2015-03-09 10:04:12 -0700

    Update fetch log script
    Signed-off-by: Atul Kshirsagar <atul.kshirsagar@gmail.com>

--- scripts/diego1/fetch_cell_logs.sh
+++ scripts/diego1/fetch_cell_logs.sh
@@ -1,9 +1,8 @@
 source $(dirname $0)/bosh_wrapper.sh
 
-if [ -z "$4" ]; then
-  echo "Usage: $0 <num_cells> <download_dir> <start_timestamp> <end_timestamp>"
+if [ -z "$2" ]; then
+  echo "Usage: $0 <num_cells> <download_dir>"
   echo "       <num_cells> must be one of '10', '20', '50', or '100'"
-  echo "       the timestamps are seconds since epoch, e.g 1425061818.123456993"
   exit 1
 fi
 
@@ -13,6 +12,7 @@ num_cells=$1
 download_dir=$2
 start_timestamp=$3
 end_timestamp=$4
+output_file=$5
 
 fast_bosh target diego1
 fast_bosh deployment ~/workspace/deployments-runtime/diego-1/deployments/diego-${num_cells}-cell.yml
@@ -33,8 +33,3 @@ for job in cell_z1 cell_z2; do
     wait
   done
 done
-
-veritas chug-unify --min=${start_timestamp} --max=${end_timestamp} ${download_dir}/*/executor/executor.stdout* ${download_dir}/*/rep/rep.stdout* ${download_dir}/*/receptor/receptor.stdout* > ${download_dir}/unity.log
-
-cd /Users/pivotal/go/src/github.com/onsi/cicerone
-go run main.go fezzik-tasks ${download_dir}/unity.log
