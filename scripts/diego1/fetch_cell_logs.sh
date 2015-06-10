e4af822404a16b2f3e869f943f1f335ad2a7c08a
Amit Gupta <agupta@pivotal.io>
2015-06-09 18:08:40 -0700

    Update log fetcher script to get receptor logs off access VMs [#86856698]

--- scripts/diego1/fetch_cell_logs.sh
+++ scripts/diego1/fetch_cell_logs.sh
@@ -16,6 +16,28 @@ fast_bosh deployment ~/workspace/deployments-runtime/diego-1/deployments/${num_c
 
 mkdir -p ${download_dir}
 
+# Get receptor logs, 2 VMs at a time
+# Assuming there are num_cells/5 access VMs split evenly across 2 zones
+num_access_per_zone=$((${num_cells} / 10))
+for index in $(sec 0 $((${num_access_per_zone} - 1))); do
+  for job in access_z1 access_z2; do
+    (
+      vm_log_dir="${download_dir}/${job}-${index}"
+      mkdir -p ${vm_log_dir}
+      if [ "$(ls -A ${vm_log_dir})" ]; then
+        echo "Already populated ${vm_log_dir}, skipping..."
+      else
+        fast_bosh logs $job $index --only 'receptor/receptor.stdout*' --dir ${vm_log_dir}
+        cd ${vm_log_dir}
+        tar -xzvf *
+        gunzip -r .
+      fi
+    ) &
+  done
+done
+
+# Get rep and garden-linux logs, 5 VMs at a time
+# Assuming there are num_cells cell VMs split evenly across 2 zones
 for job in cell_z1 cell_z2; do
   for i in $(seq 0 $((${num_cells} / 10 - 1))); do
     for index in $(seq $(($i * 5)) $(($i * 5 + 4))); do
@@ -25,7 +47,7 @@ for job in cell_z1 cell_z2; do
         if [ "$(ls -A ${vm_log_dir})" ]; then
           echo "Already populated ${vm_log_dir}, skipping..."
         else
-          fast_bosh logs $job $index --only 'garden-linux/garden-linux.stdout*,executor/executor.stdout*,rep/rep.stdout*,receptor/receptor.stdout*' --dir ${vm_log_dir}
+          fast_bosh logs $job $index --only 'garden-linux/garden-linux.stdout*,rep/rep.stdout*' --dir ${vm_log_dir}
           cd ${vm_log_dir}
           tar -xzvf *
           gunzip -r .
