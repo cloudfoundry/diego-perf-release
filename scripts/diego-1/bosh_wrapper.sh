8440d1fd2630ab3fdf4b220725505386af885246
Luan Santos <lsantos@pivotal.io>
2016-07-25 09:24:25 -0700

    Clean up old unused parts of the release
     - bump bbs cfhttp cflager clock consuladapter diego-stress-tests fezzik lager locket runtimeschema gvt bbs cf-test-helpers cf_http consuladapter diego-stress-tests fezzik locket dropsonde gunk sonde-go protobuf clock lager go.uuid godep net
     - add new pusher
     - add new orchestrator
     - update fezzik dependencies
     - delete garden tests
     - delete old stress tests
     - update cf/golang blobs
     - update manifest generation so that it works generically
    
    Submodule src/code.cloudfoundry.org/bbs 0000000...9314c42 (new submodule)
    Submodule src/code.cloudfoundry.org/cfhttp 0000000...9f848b8 (new submodule)
    Submodule src/code.cloudfoundry.org/cflager 0000000...3b0406d (new submodule)
    Submodule src/code.cloudfoundry.org/clock 0000000...da82951 (new submodule)
    Submodule src/code.cloudfoundry.org/consuladapter 0000000...cec33a3 (new submodule)
    Submodule src/code.cloudfoundry.org/diego-stress-tests 0000000...b558a4f (new submodule)
    Submodule src/code.cloudfoundry.org/fezzik 0000000...162b1a9 (new submodule)
    Submodule src/code.cloudfoundry.org/lager 0000000...62951a8 (new submodule)
    Submodule src/code.cloudfoundry.org/locket 0000000...eb32dc4 (new submodule)
    Submodule src/code.cloudfoundry.org/runtimeschema 0000000...eed404e (new submodule)
    Submodule src/github.com/FiloSottile/gvt 0000000...02e2422 (new submodule)
    Submodule src/github.com/cloudfoundry/dropsonde 0000000...3899ece (new submodule)
    Submodule src/github.com/cloudfoundry/sonde-go 0000000...27b0c70 (new submodule)
    Submodule src/github.com/gogo/protobuf 2093b57..5d45959 (rewind):
      < fixed another fuzz test
      < fuzz tests found unchecked errors
      < fixed some fuzz tests
      < more exposed methods in the generator
      < Merge pull request #104 from ceram1/comment-from-ext
      < merge 535a10468679b4cf155f6a7afdf53b554633fc09 from golang/protobuf
      < made protoc-gen-gogo also use the vanity/command like all the other commands
      < moved protoc-gen-combo and protoc-min-version and the version library to the root
      < gostring prints less nil values
      < Merge branch 'master' into oneof
      < print out seeds for failing size tests
      < oneof: customname is starting to work
      < oneof: casttype is starting to work
      < oneof: proto3 marshalling fixed
      < all the shadows
      < vet shadow
      < vet
      < oneof: dont check errors that are always nil
      < oneof: more tests and customtype is starting to work
      < oneof: proto3 marshal fix
      < oneof: fixed populate maxfield bug
      < print out seed for failing tests
      < oneof: better wireType errors and verbose equal fix
      < merge 1dceb1a2654bdc74ca97ad91f71f500eecc96269 from golang/protobuf
      < merge 6a514e697ffedcf5d6164f65cb159cf58602c599 from golang/protobuf
      < merge 9fa1be771c31716598ee886c52af380246058132 from golang/protobuf
      < merge 483e329c7aa200b7e10dcd9cf3aa513fb77c1d01 from golang/protobuf
      < merge ef197e1a5f817199945aa5c661145b2c5f4b963f from golang/protobuf
      < merge 61b9593c6ea6768fd683086d347fb6196deb08a2 from golang/protobuf
      < merge a57d29119a1033efed1843acbb96f5d6bc4ce9a0 from golang/protobuf
      < merge 73ca790d138754f6ea69987ab1edfc80efa53905 from golang/protobuf
    Submodule src/github.com/satori/go.uuid 0000000...0aa62d5 (new submodule)
    Submodule src/github.com/tools/godep 0000000...3c0ccb9 (new submodule)
    Submodule src/golang.org/x/net 0000000...4d38db7 (new submodule)
    
    [#126524873]
    
    Signed-off-by: Anoop Gopalakrishnan <agopalakrishnan@pivotal.io>

--- scripts/diego-1/bosh_wrapper.sh
+++
@@ -1,4 +0,0 @@
-function fast_bosh () {
-  BUNDLE_GEMFILE=~/workspace/diego-perf-release/scripts/diego-1/bosh.Gemfile bundle exec bosh $@
-}
-
---
+++ scripts/get_paths.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+absolute_path() {
+  (cd $1 && pwd)
+}
+
+scripts_path=$(absolute_path `dirname $0`)
+
+DIEGO_PERF_RELEASE_DIR=${DIEGO_PERF_RELEASE_DIR:-$(absolute_path $scripts_path/..)}
+CF_RELEASE_DIR=${CF_RELEASE_DIR:-$(absolute_path $DIEGO_PERF_RELEASE_DIR/../cf-release)}
+DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$(absolute_path $DIEGO_PERF_RELEASE_DIR/../diego-release)}
+
+CF_MANIFESTS_DIR=$CF_RELEASE_DIR/bosh-lite/deployments
+DIEGO_MANIFESTS_DIR=$DIEGO_RELEASE_DIR/bosh-lite/deployments
+DIEGO_PERF_MANIFESTS_DIR=$DIEGO_PERF_RELEASE_DIR/bosh-lite/deployments
+
+echo CF_RELEASE_DIR=$CF_RELEASE_DIR
+echo CF_MANIFESTS_DIR=$CF_MANIFESTS_DIR
+echo DIEGO_RELEASE_DIR=$DIEGO_RELEASE_DIR
+echo DIEGO_MANIFESTS_DIR=$DIEGO_MANIFESTS_DIR
+echo DIEGO_PERF_RELEASE_DIR=$DIEGO_PERF_RELEASE_DIR
+echo DIEGO_PERF_MANIFESTS_DIR=$DIEGO_PERF_MANIFESTS_DIR
